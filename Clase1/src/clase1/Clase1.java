4af8b1452b98541f3028fa8d9311485f5f5c2cd0
LC1300823\LC1300XXXX <s.cocca.u@gmail.com>
2017-08-07 22:24:53 -0400

    este es el commit inicial

---
+++ Clase1/src/clase1/Clase1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1;
+
+/**
+ *
+ * @author LC1300XXXX
+ */
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+                
+    }
+           
+}
